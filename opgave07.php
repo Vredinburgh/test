39f5d1198855508446e1e733a5891564a98c4df7
Vredinburgh <ian@Ians-MacBook-Pro.local>
2016-11-17 20:36:42 +0100

    iets

---
+++ opgave07.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<!--[Naam: Ian Vredenburg Studentnummer: 1062460]-->
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Opgave 7</title>
+    </head>
+    <body>
+        <?php
+        /* Gebruik onderstaande variabelen in de uitwerking */
+        $aantalWeken = 1;
+
+        /* Begin uitwerking */
+        for ($i = 1; $i <= $aantalWeken; $i++){
+          print(($i * 7)." dagen, aantal weken is ".$i."<br>");
+        }
+
+        /* Einde uitwerking */
+        ?>
+    </body>
+</html>
